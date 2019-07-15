9d4f89f900920a3ac9788c3574bdf86b6f98f4de
dbudimir <dav.budimir@gmail.com>
2019-07-15 10:21:07 -0400

    Initial commit, set up done, ready to build components.

---
+++ components/Layout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Nav from './Nav';
+
+class Layout extends Component {
+   render() {
+      return (
+         <div>
+            <Nav />
+         </div>
+      );
+   }
+}
+
+export default Layout;
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Layout from '../components/Layout';
+
+class index extends Component {
+   render() {
+      return (
+         <div>
+            <Layout />
+            <p>testing</p>
+         </div>
+      );
+   }
+}
+
+export default index;
