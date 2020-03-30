68033d7cf4b12c87a1d630556d0144a9a7a938ae
dbudimir <dav.budimir@gmail.com>
2020-03-29 22:00:51 -0400

    Refactored feedback interface and mobile optimized.

--- components/sitewide/Feedback.js
+++ components/sitewide/Feedback.js
@@ -9,6 +9,11 @@ import { TwitterShareButton, TwitterIcon, EmailIcon } from 'react-share';
 import styled from 'styled-components';
 
 // Styles
+const size = {
+  tablet: 'only screen and (max-width : 768px)',
+  phone: 'only screen and (max-width : 548px)',
+};
+
 const FeedbackIconContainer = styled.div`
   display: block;
   position: fixed;
@@ -16,6 +21,7 @@ const FeedbackIconContainer = styled.div`
   left: 10px;
   font-size: 16px;
   font-family: Nunito;
+  color: #ffffff;
   cursor: pointer;
 
   * {
@@ -23,109 +29,80 @@ const FeedbackIconContainer = styled.div`
     transition-timing-function: ease-in-out;
   }
 
-  img {
-    height: 24px;
-    width: 24px;
-    display: block;
-  }
-
-  .feedback-component-closed {
-    display: flex;
-    border-radius: 100px;
-    box-shadow: 0 11px 40px 0 rgba(0, 0, 0, 0.25), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
-    padding: 4px;
+  .feedback-container {
     background: #262f40;
-    cursor: pointer;
+    width: max-content;
+    display: flex;
+    align-items: center;
+    padding: 12px;
+    border-radius: 12px;
+    border: 2px solid transparent;
+    box-shadow: 0 11px 40px 0 rgba(0, 0, 0, 0.25), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
 
     span {
-      display: block;
-      color: #ffffff;
-      width: 0px;
-      height: 0px;
-      opacity: 0;
-      margin-left: 0px;
+      margin-right: 12px;
     }
-  }
-
-  .feedback-component-open {
-    display: flex;
-    border-radius: 12px;
-    box-shadow: 0 11px 40px 0 rgba(0, 0, 0, 0.25), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
-    align-items: unset;
-    padding: 12px;
-    background: #262f40;
-    border: 2px solid transparent;
 
     &:hover {
       border: 2px solid #8ca0f8;
     }
 
-    .feedback-modal-open {
+    .feedback-inputs {
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
       width: 400px;
-      height: max-content;
-      padding: 6px;
-      color: #ffffff;
 
       .feedback-modal-header {
-        width: 100%;
         font-size: 22px;
-        margin: 0 0 32px 0;
+        margin: 0 6px 18px 2px;
       }
 
       .react-share__ShareButton {
         width: 100%;
-        margin-bottom: 24px;
-
-        .email-feedback,
-        .twitter-feedback {
-          display: flex;
-          align-items: center;
-          justify-content: space-between;
-          border: 1px solid #ffffff;
-          border-radius: 8px;
-          padding: 0 12px;
-          font-size: 20px;
-          font-weight: 700;
-          transition-duration: 0.1s;
-
-          p {
-            transition-duration: 0.1s;
-            margin: 12px 0;
-          }
-
-          &:hover {
-            background-color: #ffffff;
-            color: #262f40;
-          }
-        }
+        display: flex;
       }
 
-      .email-input-container {
-        display: none;
-      }
-    }
-
-    .feedback-modal-open.email-input-open {
+      .twitter-feedback,
       .email-feedback {
-        border-top-left-radius: 8px;
-        border-top-right-radius: 8px;
-        border-bottom-left-radius: 0px;
-        border-bottom-right-radius: 0px;
-        background-color: #ffffff;
-        color: #262f40;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        border: 1px solid #ffffff;
+        border-radius: 8px;
+        padding: 0 12px;
+        font-size: 20px;
+        font-weight: 700;
+        transition-duration: 0.1s;
+        margin-bottom: 12px;
+        width: 100%;
+
+        &.active {
+          background-color: #ffffff;
+          color: #262f40;
+        }
+
+        p {
+          transition-duration: 0.1s;
+          margin: 12px 0;
+        }
+        &:hover {
+          background-color: #ffffff;
+          color: #262f40;
+        }
       }
 
       .email-input-container {
         display: block;
         padding: 0 12px 24px;
-        margin-top: -24px;
+        margin-top: -18px;
+        width: 100%;
         border-bottom-left-radius: 8px;
         border-bottom-right-radius: 8px;
         background: #ffffff;
 
         .form-input-label {
           margin-bottom: 12px;
-
           span {
             color: #262f40;
             display: block;
@@ -134,7 +111,6 @@ const FeedbackIconContainer = styled.div`
             font-weight: 600;
             margin: 0 0 6px 0;
           }
-
           input,
           textarea {
             width: 100%;
@@ -142,7 +118,6 @@ const FeedbackIconContainer = styled.div`
             border-radius: 6px;
             -webkit-appearance: none;
             display: block;
-
             font-size: 16px;
             padding: 6px;
             font-family: Nunito;
@@ -151,6 +126,7 @@ const FeedbackIconContainer = styled.div`
         }
 
         .send-email-button {
+          display: block;
           background: #262f40;
           width: 100%;
           margin: 0px;
@@ -158,7 +134,7 @@ const FeedbackIconContainer = styled.div`
           font-size: 18px;
           padding: 12px 6px;
           box-sizing: border-box;
-          margin-top: 12px;
+          margin-top: 18px;
           border-radius: 6px;
           font-weight: 700;
           cursor: pointer;
@@ -166,13 +142,30 @@ const FeedbackIconContainer = styled.div`
       }
     }
 
-    span {
+    #close-feedback-button {
+      align-self: baseline;
+    }
+
+    @media ${size.tablet} {
+      position: absolute;
+      bottom: 0px;
+      width: calc(100vw - 50px);
+      justify-content: space-between;
+
+      .feedback-inputs {
+        width: 100%;
+      }
+    }
+  }
+
+  .feedback-component-closed {
+    background: #262f40;
+    padding: 4px;
+    border-radius: 100px;
+    box-shadow: 0 11px 40px 0 rgba(0, 0, 0, 0.25), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+
+    img {
       display: block;
-      color: #ffffff;
-      width: 280px;
-      height: 46px;
-      opacity: 1;
-      margin: 0px 6px;
     }
   }
 `;
@@ -182,13 +175,17 @@ export default class Index extends Component {
     super(props);
     this.state = {
       ...props,
-      feedbackClass: 'feedback-component-open',
-      feedbackModalClass: 'feedback-modal-closed',
-      emailInputClass: 'email-input-closed',
+      feedbackDisplay: 'feedbackMessage',
+      emailInputDisplay: false,
+      emailFeedbackClass: 'email-feedback',
     };
   }
 
-  componentDidMount() {}
+  componentDidMount = () => {
+    this.setState({
+      pageURL: window.location.href,
+    });
+  };
 
   updateState = event => {
     const { target } = event;
@@ -219,138 +216,138 @@ export default class Index extends Component {
 
   openEmailUI = () => {
     this.setState(prevState => ({
-      emailInputClass:
-        prevState.emailInputClass === 'email-input-closed'
-          ? 'email-input-open'
-          : 'email-input-closed',
+      emailInputDisplay: !prevState.emailInputDisplay,
+      emailFeedbackClass:
+        prevState.emailInputClass === 'email-feedback active'
+          ? 'email-feedback'
+          : 'email-feedback active',
     }));
   };
 
   openFeedbackModal = e => {
-    e.preventDefault();
-    if (e.target.id === 'email-button') {
-      this.openEmailUI();
-    } else if (e.target.id !== 'email-input') {
+    if (e.target.id === 'close-feedback-button') {
+      this.setState({
+        feedbackDisplay: 'infoIcon',
+      });
+    } else if (e.target.id === 'open-feedback-ui') {
+      this.setState({
+        feedbackDisplay: 'feedbackMessage',
+      });
+    } else {
       this.setState(prevState => ({
         feedbackModalClass:
-          prevState.feedbackModalClass === 'feedback-modal-closed'
-            ? 'feedback-modal-open'
-            : 'feedback-modal-closed',
+          prevState.feedbackModalClass === 'feedback-modal-open'
+            ? 'feedback-modal-closed'
+            : 'feedback-modal-open',
+        feedbackDisplay: 'feedbackUI',
       }));
     }
   };
 
-  minimizeFeedback = className => {
-    this.setState(prevState => ({
-      feedbackClass:
-        prevState.feedbackClass === 'feedback-component-closed'
-          ? 'feedback-component-open'
-          : 'feedback-component-closed',
-      feedbackModalClass:
-        prevState.feedbackModalClass === 'feedback-modal-closed'
-          ? 'feedback-modal-open'
-          : 'feedback-modal-closed',
-    }));
-  };
-
-  openCloseFeedback = className => {
-    this.setState({
-      feedbackClass: className,
-    });
-  };
-
   render() {
     const {
-      feedbackClass,
-      feedbackModalClass,
-      emailInputClass,
+      feedbackDisplay,
+      emailInputDisplay,
+      emailFeedbackClass,
       email,
       emailMessage,
       pageURL,
     } = this.state;
 
-    // Determines which SVG icon to show
-    const feedBackIcon =
-      feedbackClass === 'feedback-component-open' ? (
-        // Show when page loads
-        <img
-          src="/static/icons/close.svg"
-          alt="Share Feedback Icon"
-          onClick={e => this.minimizeFeedback('feedback-component-closed')}
-        />
-      ) : (
-        // Show after user has hidden notification
-        <img src="/static/icons/info.svg" alt="Share Feedback Icon" />
-      );
+    console.log(this.state);
 
-    // Shows or hides modal on click
-    const feedBackModal =
-      feedbackModalClass === 'feedback-modal-open' ? (
-        // Show when user clicks with intent to submit feedback
-        <div className={`${feedbackClass}`} onClick={e => this.openFeedbackModal(e)}>
-          <div className={`${feedbackModalClass} ${emailInputClass}`}>
-            {/* Intro text */}
-            <span className="feedback-modal-header">
-              Something wrong? Let us know how you expected this page to work.
-            </span>
-            {/* Twitter button */}
-            <TwitterShareButton url={pageURL} title="@mealdig" hashtags={['feedback']}>
-              <div className="twitter-feedback">
-                <p> Submit Via Twitter </p>
-                <TwitterIcon size={32} round />
-              </div>
-            </TwitterShareButton>
-            {/* Email button */}
-            <div id="email-button" className="react-share__ShareButton">
-              <div id="email-button" className="email-feedback">
-                <p id="email-button"> Send Directly </p>
-                <EmailIcon id="email-button" size={32} round />
-              </div>
-            </div>
-            {/* Email input */}
-            <div id="email-input" className="email-input-container">
-              <div id="email-input" className="form-input-label">
-                <span id="email-input">Email Address (optional)</span>
-                <input
-                  id="email-input"
-                  name="email"
-                  onChange={this.updateState}
-                  value={email || ''}
-                  type="text"
-                  placeholder="Enter your email"
-                />
-              </div>
-              <div id="email-input" className="form-input-label">
-                <span id="email-input">Message</span>
-                <textarea
-                  id="email-input"
-                  name="emailMessage"
-                  onChange={this.updateState}
-                  value={emailMessage || ''}
-                  rows="4"
-                  placeholder="Share your thoughts..."
-                />
-              </div>
-              <span className="send-email-button" onClick={this.sendFeedbackEmail}>
-                Submit Feedback
-              </span>
-            </div>
+    let emailInputContainer = '';
+    if (emailInputDisplay === true) {
+      emailInputContainer = (
+        <div className="email-input-container">
+          <div className="form-input-label">
+            <span>Email Address (optional)</span>
+            <input
+              name="email"
+              onChange={this.updateState}
+              value={email || ''}
+              type="text"
+              placeholder="Enter your email"
+            />
           </div>
-          <img src="/static/icons/close.svg" alt="Share Feedback Icon" />
-        </div>
-      ) : (
-        // Show when page loads or when user hovers over icon to see notificaton.
-        <div
-          className={`${feedbackClass}`}
-          onMouseEnter={e => this.openCloseFeedback('feedback-component-open')}
-          onMouseLeave={e => this.openCloseFeedback('feedback-component-closed')}
-          onClick={this.openFeedbackModal}
-        >
-          <span>We're a new website. Click here to share your feedback at anytime.</span>
-          {feedBackIcon}
+          <div className="form-input-label">
+            <span>Message</span>
+            <textarea
+              name="emailMessage"
+              onChange={this.updateState}
+              value={emailMessage || ''}
+              rows="4"
+              placeholder="Share your thoughts..."
+            />
+          </div>
+          <span className="send-email-button" onClick={this.sendFeedbackEmail}>
+            Submit Feedback
+          </span>
         </div>
       );
+    } else {
+      emailInputContainer = '';
+    }
 
-    return <FeedbackIconContainer>{feedBackModal}</FeedbackIconContainer>;
+    let feedbackIcon = '';
+    switch (feedbackDisplay) {
+      case 'infoIcon':
+        feedbackIcon = (
+          <>
+            <div className="feedback-component-closed" onClick={e => this.openFeedbackModal(e)}>
+              <img id="open-feedback-ui" src="/static/icons/info.svg" alt="Share Feedback Icon" />
+            </div>
+          </>
+        );
+        break;
+      case 'feedbackMessage':
+        feedbackIcon = (
+          <>
+            <div className="feedback-container" onClick={this.openFeedbackModal}>
+              <span>We're a new website. Click here to share your feedback at anytime.</span>
+              <img
+                id="close-feedback-button"
+                src="/static/icons/close.svg"
+                alt="Share Feedback Icon"
+              />
+            </div>
+          </>
+        );
+        break;
+      case 'feedbackUI':
+        feedbackIcon = (
+          <div className="feedback-container" onClick={e => this.openFeedbackModal(e)}>
+            <div className="feedback-inputs">
+              {/* Intro text */}
+              <span className="feedback-modal-header">
+                Something wrong? Tell us how you expected this page to work.
+              </span>
+              {/* Twitter button */}
+              <TwitterShareButton url={`${pageURL}`} title="@mealdig" hashtags={['feedback']}>
+                <div className="twitter-feedback">
+                  <p> Submit Via Twitter </p>
+                  <TwitterIcon size={32} round />
+                </div>
+              </TwitterShareButton>
+              {/* Email button */}
+              <div className={emailFeedbackClass} onClick={e => this.openEmailUI(e)}>
+                <p> Send Directly </p>
+                <EmailIcon i size={32} round />
+              </div>
+              {emailInputContainer}
+            </div>
+            <img
+              id="close-feedback-button"
+              src="/static/icons/close.svg"
+              alt="Share Feedback Icon"
+            />
+          </div>
+        );
+        break;
+      default:
+        break;
+    }
+
+    return <FeedbackIconContainer>{feedbackIcon}</FeedbackIconContainer>;
   }
 }
